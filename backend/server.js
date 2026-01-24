38d831309f925e815e6adb6bed2eeda479a30855
ZainabNaeem10 <zainabnaeem1004@gmail.com>
2026-01-24 16:00:00 +0500

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,11 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+// Home route
+app.get("/", (req, res) => {
+  res.send("Healthcare Appointment System Backend is running!");
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
