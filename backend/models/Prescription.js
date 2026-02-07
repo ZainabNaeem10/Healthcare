52825135ea208ad19e9952982c2faa11af530cf8
ZainabNaeem10 <zainabnaeem1004@gmail.com>
2026-02-07 15:39:00 +0500

    Downloaded button

--- backend/models/Prescription.js
+++ backend/models/Prescription.js
@@ -5,6 +5,7 @@ const prescriptionSchema = new mongoose.Schema({
   doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor", required: true },
   medicines: [{ name: String, dosage: String, duration: String }],
   notes: String
+  downloaded: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Prescription", prescriptionSchema);
