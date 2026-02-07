64465f409600ba26fd35ed8cc5d859dec18cbb64
ZainabNaeem10 <zainabnaeem1004@gmail.com>
2026-02-07 15:43:00 +0500

    Downloaded button resolved

--- backend/models/Prescription.js
+++ backend/models/Prescription.js
@@ -4,7 +4,7 @@ const prescriptionSchema = new mongoose.Schema({
   patientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor", required: true },
   medicines: [{ name: String, dosage: String, duration: String }],
-  notes: String
+  notes: String,
   downloaded: { type: Boolean, default: false }
 }, { timestamps: true });
 
