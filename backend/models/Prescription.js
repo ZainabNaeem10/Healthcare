a3063b64acf41d35674ecda64985ad3885993f20
ZainabNaeem10 <zainabnaeem1004@gmail.com>
2026-02-07 16:18:26 +0500

    updated

--- backend/models/Prescription.js
+++ backend/models/Prescription.js
@@ -1,10 +1,9 @@
-const mongoose = require("mongoose");
-
 const prescriptionSchema = new mongoose.Schema({
   patientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor", required: true },
   medicines: [{ name: String, dosage: String, duration: String }],
   notes: String,
+  pdfUrl: String,
   downloaded: { type: Boolean, default: false }
 }, { timestamps: true });
 
